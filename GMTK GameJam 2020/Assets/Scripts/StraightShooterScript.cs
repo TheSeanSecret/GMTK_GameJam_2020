a40a752e7b84d5e04f7566c4c056b506eaacb0b9
TheSeanSecret <sean@timogan.name>
2020-07-12 02:11:27 +0200

    9
    - 21 total levels!
    - New SFX

---
+++ GMTK GameJam 2020/Assets/Scripts/StraightShooterScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightShooterScript : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform muzzle;
+
+    public float fireRate = 0.5F;
+    private float nextFire = 0.0F;
+
+    void Update()
+    {
+        
+
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(bullet, muzzle.position, muzzle.rotation);      // Instansierar enemy bullet muzzle med mellanrum
+        }
+    }
+}
